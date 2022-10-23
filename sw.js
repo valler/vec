0b8e4cbcac251339a314e0020b6ab32c8108fb22
valler <hi@valerij.me>
2022-10-23 13:10:44 +0200

    Implement service worker stale while revalidate

--- sw.js
+++ sw.js
@@ -1,41 +1,13 @@
 const cacheName = "c4", files = [
-    "./",
     "offline.html",
     "base.css",
     "style.css",
     "app.js",
-    "app.js.map",
     "favicon.ico",
     "icon.svg",
     "icon192.png",
     "icon512.png",
     "font/Source_Sans_3/SourceSans3-VariableFont_wght.ttf",
-    "font/Source_Sans_3/SourceSans3-Italic-VariableFont_wght.ttf",
-    "font/Source_Code_Pro/SourceCodePro-VariableFont_wght.ttf",
-    "font/Source_Code_Pro/SourceCodePro-Italic-VariableFont_wght.ttf",
-    "installation/",
-    "learn/",
-    "learn/area/",
-    "learn/composition/",
-    "learn/direction/",
-    "learn/equality/",
-    "learn/identity/",
-    "learn/interpolation/",
-    "learn/inverse/",
-    "learn/length/",
-    "learn/modules/",
-    "learn/motion/",
-    "learn/projection/",
-    "learn/reduction/",
-    "learn/reflection/",
-    "lib/js/vec.js",
-    "lib/js/vec2.js",
-    "lib/js/vec3.js",
-    "lib/js/vec4.js",
-    "lib/js/vec.js.map",
-    "lib/js/vec2.js.map",
-    "lib/js/vec3.js.map",
-    "lib/js/vec4.js.map",
 ];
 self.addEventListener("install", (event) => {
     self.skipWaiting();
@@ -55,9 +27,19 @@ self.addEventListener("fetch", (event) => {
     event.respondWith((async () => {
         const { request } = event;
         const cached = await caches.match(request);
-        if (cached)
+        if (cached) {
+            fetch(request).then((async (response) => {
+                (await caches.open(cacheName))
+                    .put(request, response.clone());
+            }));
             return cached;
-        return fetch(request);
+        }
+        else {
+            const fetched = await fetch(request);
+            (await caches.open(cacheName))
+                .put(request, fetched.clone());
+            return fetched;
+        }
     })());
 });
 export {};
--- sw.ts
+++ sw.ts
@@ -1,45 +1,17 @@
-export type {};
+export type { };
 declare const self: ServiceWorkerGlobalScope;
 const
   cacheName = "c4",
   files = [
-    "./",
     "offline.html",
     "base.css",
     "style.css",
     "app.js",
-    "app.js.map",
     "favicon.ico",
     "icon.svg",
     "icon192.png",
     "icon512.png",
     "font/Source_Sans_3/SourceSans3-VariableFont_wght.ttf",
-    "font/Source_Sans_3/SourceSans3-Italic-VariableFont_wght.ttf",
-    "font/Source_Code_Pro/SourceCodePro-VariableFont_wght.ttf",
-    "font/Source_Code_Pro/SourceCodePro-Italic-VariableFont_wght.ttf",
-    "installation/",
-    "learn/",
-    "learn/area/",
-    "learn/composition/",
-    "learn/direction/",
-    "learn/equality/",
-    "learn/identity/",
-    "learn/interpolation/",
-    "learn/inverse/",
-    "learn/length/",
-    "learn/modules/",
-    "learn/motion/",
-    "learn/projection/",
-    "learn/reduction/",
-    "learn/reflection/",
-    "lib/js/vec.js",
-    "lib/js/vec2.js",
-    "lib/js/vec3.js",
-    "lib/js/vec4.js",
-    "lib/js/vec.js.map",
-    "lib/js/vec2.js.map",
-    "lib/js/vec3.js.map",
-    "lib/js/vec4.js.map",
   ];
 self.addEventListener("install", (event: { waitUntil: (x: Promise<void>) => void; }) => {
   self.skipWaiting();
@@ -56,11 +28,25 @@ self.addEventListener("activate", (event: { waitUntil: (x: Promise<void>) => voi
     );
   })());
 });
-self.addEventListener("fetch", (event: { respondWith: any; request: any; }) => {
+type arg = {
+  respondWith: (x: Response | Promise<Response>) => void;
+  request: Request;
+};
+self.addEventListener("fetch", (event: arg) => {
   event.respondWith((async () => {
     const { request } = event;
     const cached = await caches.match(request);
-    if (cached) return cached;
-    return fetch(request);
+    if (cached) {
+      fetch(request).then((async response => {
+        (await caches.open(cacheName))
+          .put(request, response.clone());
+      }));
+      return cached;
+    } else {
+      const fetched = await fetch(request);
+      (await caches.open(cacheName))
+        .put(request, fetched.clone());
+      return fetched;
+    }
   })());
 });
